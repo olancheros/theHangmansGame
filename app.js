8712d38d644cca3a02ebea33288c67d6c7316502
olancheros <olancheros@gmail.com>
2022-05-29 19:16:11 -0500

    all the needed logic for the game implementation it is done.

--- app.js
+++ app.js
@@ -3,12 +3,135 @@
 ;(function(){
     'use strict'
 
-    var game = {
-        word: "ALURA",
-        state:1,
-        guessed: ['A', 'L'],
-        failed: ['B', 'J', 'K', 'C']
-    }
+    var animalList = [
+        'ABEJA',
+        'AGUILA',
+        'ALMEJA',
+        'ARAÑA',
+        'AVESTRUZ',
+        'AVISP',
+        'BABOSA',
+        'BALLENA AZUL',
+        'BISONTE',
+        'BUFALO',
+        'BUHO',
+        'BUITRE',
+        'BURRO',
+        'CABALLITO DE MAR',
+        'CABALLO',
+        'CABRA',
+        'CALAMAR',
+        'CAMALEON',
+        'CAMARON',
+        'CAMELLO',
+        'CANARIO',
+        'CANGREJO',
+        'CARACOL',
+        'CASTOR',
+        'CEBRA',
+        'CERDO',
+        'CHINCHE',
+        'CIEMPIES',
+        'CIERVO',
+        'CIGARRA',
+        'COBRA',
+        'COCHINILLA',
+        'COLIBRI',
+        'COMADREJA',
+        'CONDOR',
+        'CONEJO',
+        'CORALES',
+        'CUCARACHA',
+        'DELFIN',
+        'ELEFANTE',
+        'ERIZO',
+        'ESCARABAJO',
+        'ESCORPION',
+        'FAISAN',
+        'FLAMENCO',
+        'FOCA',
+        'GALLINA',
+        'GALLO',
+        'GARRAPATA',
+        'GATO',
+        'GORGOJO',
+        'GORILA',
+        'GRILLO',
+        'GUEPARDO',
+        'GUSANO',
+        'HAMSTER',
+        'HIENA',
+        'HIPOPOTAMO',
+        'HORMIGA',
+        'JABALI',
+        'JAGUAR',
+        'JIRAFA',
+        'KOALA',
+        'LAGARTO',
+        'LANGOSTA',
+        'LANGOSTINO',
+        'LAPA',
+        'LARVA',
+        'LEON',
+        'LEON MARINO',
+        'LIBELULA',
+        'LLAMA',
+        'LOBO',
+        'LOMBRIZ',
+        'LORO',
+        'LUCIERNAGA',
+        'MANATI',
+        'MANTIS RELIGIOSA',
+        'MAPACHE',
+        'MARIPOSA',
+        'MARIQUITA',
+        'MEDUSAS',
+        'MEJILLON',
+        'MONO',
+        'MOSCA',
+        'MOSQUITO',
+        'MURCIELAGO',
+        'ÑANDU',
+        'NUTRIA',
+        'ORCA',
+        'OSTRA',
+        'PALOMA',
+        'PANDA',
+        'PATO',
+        'PAVO REAL',
+        'PELICANO',
+        'PERRO',
+        'PINGUINO',
+        'PIOJO',
+        'POLILLA',
+        'POLIPO',
+        'PUERCOESPIN',
+        'PULGA',
+        'PULPO',
+        'PUMA',
+        'RANA',
+        'RATON',
+        'RENO',
+        'RINOCERONTE',
+        'SALAMANDRA',
+        'SALTAMONTES',
+        'SANGUIJUELA',
+        'SAPO',
+        'SERPIENTE',
+        'TAPIR',
+        'TEJON',
+        'TIBURON',
+        'TIGRE',
+        'TOPO',
+        'TORO',
+        'TUCAN',
+        'VACA',
+        'ZORRILLO',
+        'ZORRO'
+    ]
+
+    var game = null;
+    var finalized = false;
 
     var $html = {
         man: document.getElementById('man'),
@@ -102,13 +225,63 @@
     window.onkeypress = function guessLetter(e) {
         var letter = e.key;
         letter = letter.toUpperCase();
-        if (/[^A-ZÑ]/.test(letter)) {
+        if (/[^A-ZÑ ]/.test(letter)) {
             return
         }
+
         guess(game, letter);
+        var state = game.state;
+        if (state == 8 && !finalized) {
+            setTimeout(winMsgAlert, 500)
+            finalized = true;
+        } else if (state == 1 && !finalized) {
+            let word = game.word;
+            let fn = failMsgAlert.bind(undefined, word);
+            setTimeout(fn, 500);
+            finalized = true;
+        }
         draw(game);
     }
 
-    draw(game);
+    window.newGame = function newGame() {
+        var word = randomWord();
+        game = {};
+        game.word = word;
+        game.state = 7;
+        game.guessed = [];
+        game.failed = [];
+        finalized = false;
+        draw(game);
+        console.log(game);
+    }
+
+    window.quitGame = function quitGame() {
+        var state = game.state
+        if (state !=  7  && state != 1 && !finalized) {
+            let word = game.word;
+            let fn = quitMsgAlert.bind(undefined, word);
+            setTimeout(fn, 500);
+            finalized = true;
+        }
+    }
+
+    function randomWord() {
+        var index = Math.trunc(Math.random() * animalList.length);
+        return animalList[index];
+    }
+
+    function winMsgAlert() {
+        alert("Congratulations, you have won!")
+    }
+
+    function failMsgAlert(word) {
+        alert("Sorry, you have lost... The right word was: " + word)
+    }
+
+    function quitMsgAlert(word) {
+        alert("Loser, you have quit... The rigth  word was: "  + word)
+    }
+
+    newGame();
 
 }())
